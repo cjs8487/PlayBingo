070aac21209eb5a9f8f1b4016aec1f9143fdedb2
cjs8487 <cjschaefer1999@gmail.com>
2024-02-07 19:31:48 -0600

    client secret reset

--- api/src/routes/oauth/OAuth.ts
+++ api/src/routes/oauth/OAuth.ts
@@ -5,6 +5,7 @@ import {
     deleteClient,
     getClient,
     getClients,
+    resetClientSecret,
     updateClient,
 } from '../../database/OAuth';
 import redirect from './redirect/Redirect';
@@ -64,5 +65,11 @@ oauth
         const client = await updateClient(id, name, redirects);
         res.status(200).json(client);
     });
+
+oauth.post('/:id/resetSecret', async (req, res) => {
+    const { id } = req.params;
+    const newSecret = await resetClientSecret(id);
+    res.status(200).json(newSecret);
+});
 
 export default oauth;
