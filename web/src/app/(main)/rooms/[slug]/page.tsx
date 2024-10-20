1afd24f6bb806ce743a000681e3ae88553408bc2
cjs8487 <cjschaefer1999@gmail.com>
2024-10-19 19:38:41 -0500

    first pass at breakpoint based layouts

--- web/src/app/(main)/rooms/[slug]/page.tsx
+++ web/src/app/(main)/rooms/[slug]/page.tsx
@@ -1,36 +1,17 @@
 'use client';
 import Board from '@/components/board/Board';
-import ColorSelect from '@/components/room/ColorSelect';
 import RoomChat from '@/components/room/RoomChat';
 import RoomInfo from '@/components/room/RoomInfo';
 import RoomLogin from '@/components/room/RoomLogin';
-import { ConnectionStatus, RoomContext } from '@/context/RoomContext';
-import Refresh from '@mui/icons-material/Refresh';
-import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    IconButton,
-    Link,
-    Typography,
-} from '@mui/material';
-import NextLink from 'next/link';
-import { useContext } from 'react';
+import { ConnectionStatus, useRoomContext } from '@/context/RoomContext';
+import { Box, Container } from '@mui/material';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import PlayerList from '../../../../components/room/PlayerList';
-import Timer from '../../../../components/room/racetime/Timer';
 import RacetimeCard from '../../../../components/room/racetime/RacetimeCard';
+import PlayerInfo from '../../../../components/room/PlayerInfo';
 
 export default function Room() {
-    const {
-        connectionStatus,
-        roomData,
-        nickname,
-        disconnect,
-        createRacetimeRoom,
-        updateRacetimeRoom,
-    } = useContext(RoomContext);
+    const { connectionStatus, roomData } = useRoomContext();
 
     if (connectionStatus === ConnectionStatus.UNINITIALIZED) {
         return <RoomLogin />;
@@ -44,72 +25,225 @@ export default function Room() {
     }
 
     return (
-        <Box flex="column" flexGrow={1} p={2}>
-            <AutoSizer>
-                {({ width, height }) => (
+        <AutoSizer>
+            {({ width, height }) => (
+                <>
                     <Box
-                        width={width}
-                        height={height}
-                        display="flex"
-                        flexDirection="column"
+                        sx={{
+                            display: { xs: 'flex', sm: 'none' },
+                            width: width,
+                            height: height,
+                            overflowY: 'auto',
+                            flexDirection: 'column',
+                            rowGap: 1.5,
+                            p: 1,
+                        }}
                     >
-                        <Box
-                            display="flex"
-                            columnGap={2}
-                            maxHeight="30%"
-                            pb={2}
-                        >
-                            <Box>
-                                <RoomInfo />
-                            </Box>
-                            <Box>
-                                <Card>
-                                    <CardContent>
-                                        <Box
-                                            display="flex"
-                                            alignItems="center"
-                                            flexGrow={1}
-                                        >
-                                            <Typography
-                                                variant="h6"
-                                                flexGrow={1}
-                                            >
-                                                Playing as {nickname}
-                                            </Typography>
-                                            {connectionStatus !==
-                                                ConnectionStatus.CLOSED && (
-                                                <Button onClick={disconnect}>
-                                                    Disconnect
-                                                </Button>
-                                            )}
-                                        </Box>
-                                        <Box>
-                                            <Typography>
-                                                Choose your color
-                                            </Typography>
-                                            <ColorSelect />
-                                        </Box>
-                                    </CardContent>
-                                </Card>
-                            </Box>
-                            <Box>
-                                <RacetimeCard />
-                            </Box>
-                        </Box>
-                        <Box display="flex" maxHeight="70%" columnGap={8}>
-                            <Box flexGrow={1} maxWidth="50%">
-                                <Board />
-                            </Box>
-                            <Box>
-                                <RoomChat />
-                            </Box>
-                            <Box>
-                                <PlayerList />
-                            </Box>
-                        </Box>
+                        <RoomXs />
                     </Box>
-                )}
-            </AutoSizer>
-        </Box>
+                    <Box
+                        sx={{
+                            display: { xs: 'none', sm: 'flex', md: 'none' },
+                            width: width,
+                            height: height,
+                            overflowY: 'auto',
+                            flexDirection: 'column',
+                            rowGap: 1.5,
+                            p: 1,
+                        }}
+                    >
+                        <RoomSm />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: { xs: 'none', md: 'flex', lg: 'none' },
+                            width: width,
+                            height: height,
+                            overflowY: 'auto',
+                            flexDirection: 'column',
+                            rowGap: 1.5,
+                            p: 1,
+                        }}
+                    >
+                        <RoomMd />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: { xs: 'none', lg: 'flex', xl: 'none' },
+                            width: width,
+                            height: height,
+                            overflowY: 'auto',
+                            p: 1,
+                        }}
+                    >
+                        <RoomLg />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: { xs: 'none', xl: 'flex' },
+                            width: width,
+                            height: height,
+                            overflowY: 'auto',
+                            p: 1,
+                        }}
+                    >
+                        <RoomXl />
+                    </Box>
+                </>
+            )}
+        </AutoSizer>
+    );
+}
+
+function RoomXs() {
+    return (
+        <>
+            xs
+            <Box>
+                <RoomInfo />
+            </Box>
+            <Box>
+                <RacetimeCard />
+            </Box>
+            <Box>
+                <PlayerInfo />
+            </Box>
+            <Box maxWidth="100%" maxHeight="100%">
+                <Board />
+            </Box>
+            <Box>
+                <PlayerList />
+            </Box>
+            <Box>
+                <RoomChat />
+            </Box>
+        </>
+    );
+}
+
+function RoomSm() {
+    return (
+        <>
+            small
+            <Box display="flex" columnGap={2}>
+                <Box flexGrow={1}>
+                    <RoomInfo />
+                </Box>
+                <Box>
+                    <RacetimeCard />
+                </Box>
+            </Box>
+            <Box>
+                <PlayerInfo />
+            </Box>
+            <Box maxWidth="80%" maxHeight="100">
+                <Board />
+            </Box>
+            <Box>
+                <PlayerList />
+            </Box>
+            <Box>
+                <RoomChat />
+            </Box>
+        </>
+    );
+}
+
+function RoomMd() {
+    return (
+        <>
+            <Box display="flex" columnGap={2}>
+                <Box flexGrow={1}>
+                    <RoomInfo />
+                </Box>
+                <Box>
+                    <PlayerInfo />
+                </Box>
+            </Box>
+            <Box>
+                <RacetimeCard />
+            </Box>
+            <Box
+                maxWidth="100%"
+                maxHeight="100%"
+                display="flex"
+                columnGap={1}
+                alignContent="center"
+                justifyContent="center"
+            >
+                <Box width="75%" maxHeight="100%">
+                    <Board />
+                </Box>
+                <Box>
+                    <PlayerList />
+                </Box>
+            </Box>
+            <Box px={4}>
+                <RoomChat />
+            </Box>
+        </>
+    );
+}
+
+function RoomLg() {
+    return (
+        <>
+            <Box
+                flexGrow={1}
+                maxWidth="75%"
+                maxHeight="100%"
+                display="flex"
+                alignContent="center"
+                justifyContent="center"
+            >
+                <Board />
+            </Box>
+            <Box display="flex" flexDirection="column" rowGap={1}>
+                <Box>
+                    <RoomInfo />
+                </Box>
+                <Box>
+                    <PlayerInfo />
+                </Box>
+                <Box>
+                    <RacetimeCard />
+                </Box>
+                <Box>
+                    <PlayerList />
+                </Box>
+                <Box>
+                    <RoomChat />
+                </Box>
+            </Box>
+        </>
+    );
+}
+
+function RoomXl() {
+    return (
+        <>
+            <Box flexGrow={1} maxWidth="50%" maxHeight="100%">
+                <Board />
+            </Box>
+            <Box display="flex" flexDirection="column" rowGap={1}>
+                <Box display="flex" columnGap={1}>
+                    <Box flexGrow={1}>
+                        <RoomInfo />
+                    </Box>
+                    <RacetimeCard />
+                </Box>
+                <Box>
+                    <PlayerInfo />
+                </Box>
+                <Box display="flex" columnGap={1}>
+                    <Box>
+                        <RoomChat />
+                    </Box>
+                    <Box>
+                        <PlayerList />
+                    </Box>
+                </Box>
+            </Box>
+        </>
     );
 }
---
+++ web/src/components/room/PlayerInfo.tsx
@@ -0,0 +1,25 @@
+import { Card, CardContent, Box, Typography, Button } from '@mui/material';
+import { ConnectionStatus, useRoomContext } from '../../context/RoomContext';
+import ColorSelect from './ColorSelect';
+
+export default function PlayerInfo() {
+    const { connectionStatus, nickname, disconnect } = useRoomContext();
+    return (
+        <Card>
+            <CardContent>
+                <Box display="flex" alignItems="center" flexGrow={1}>
+                    <Typography variant="h6" flexGrow={1}>
+                        Playing as {nickname}
+                    </Typography>
+                    {connectionStatus !== ConnectionStatus.CLOSED && (
+                        <Button onClick={disconnect}>Disconnect</Button>
+                    )}
+                </Box>
+                <Box>
+                    <Typography>Choose your color</Typography>
+                    <ColorSelect />
+                </Box>
+            </CardContent>
+        </Card>
+    );
+}
