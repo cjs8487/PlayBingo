573d11c229721548b5536e454f2146104712f93d
Floha <floha2011@hotmail.de>
2024-11-15 20:09:05 +0100

    UserSearch now sorts users by name and has a searchField

--- web/src/components/UserSearch.tsx
+++ web/src/components/UserSearch.tsx
@@ -10,16 +10,16 @@ import {
     ListItem,
     ListItemButton,
     ListItemIcon,
-    ListItemText,
+    ListItemText, TextField,
     useMediaQuery,
     useTheme,
 } from '@mui/material';
-import { forwardRef, useCallback, useState } from 'react';
+import {forwardRef, useCallback, useState} from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import { Virtuoso } from 'react-virtuoso';
-import { useApi } from '../lib/Hooks';
-import { alertError } from '../lib/Utils';
-import { User } from '../types/User';
+import {Virtuoso} from 'react-virtuoso';
+import {useApi} from '../lib/Hooks';
+import {alertError} from '../lib/Utils';
+import {User} from '../types/User';
 
 interface UserSearchProps {
     isOpen: boolean;
@@ -29,42 +29,54 @@ interface UserSearchProps {
 }
 
 export default function UserSearch({
-    isOpen,
-    close,
-    submit,
-    listPath,
-}: UserSearchProps) {
+                                       isOpen,
+                                       close,
+                                       submit,
+                                       listPath,
+                                   }: UserSearchProps) {
     const {
         data: users,
         isLoading,
         error,
     } = useApi<User[]>(listPath ?? '/api/users');
-
+    
     const [selected, setSelected] = useState<string[]>([]);
-
+    const [searchString, setSearchString] = useState('');
+    
     const cancel = useCallback(() => {
         setSelected([]);
+        setSearchString('')
         close();
     }, [close]);
-
+    
     const onSubmit = useCallback(() => {
         submit(selected);
         setSelected([]);
+        setSearchString('');
         close();
     }, [submit, selected, close]);
-
+    
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
+    
     if (!users || isLoading) {
         return null;
     }
-
+    
     if (error) {
         alertError("Couldn't load user list.");
         return null;
     }
-
+    
+    let listedUsers = (users as User[])
+        .toSorted((a, b) => a.username.localeCompare(b.username))
+        .filter((user) => {
+            if (!searchString || searchString.length === 0) {
+                return true
+            }
+            return user.username.startsWith(searchString) || user.username.includes(searchString)
+        })
+    
     return (
         <Dialog
             onClose={close}
@@ -74,17 +86,23 @@ export default function UserSearch({
             fullWidth
         >
             <DialogTitle>User Search</DialogTitle>
-            <DialogContent sx={{ minHeight: '300px' }}>
+            <DialogContent sx={{minHeight: '300px'}}>
+                <TextField
+                    type="text"
+                    label="Search"
+                    onChange={(e) => setSearchString(e.target.value)}
+                    sx={{ width: '33%' }}
+                />
                 <AutoSizer>
-                    {({ height, width }) => (
+                    {({height, width}) => (
                         <Virtuoso<User>
                             height={height}
                             width={width}
-                            style={{ height, width }}
+                            style={{height, width}}
                             components={{
                                 // eslint-disable-next-line react/display-name
                                 List: forwardRef(
-                                    ({ style, children }, listRef) => {
+                                    ({style, children}, listRef) => {
                                         return (
                                             <List
                                                 style={{
@@ -100,7 +118,7 @@ export default function UserSearch({
                                         );
                                     },
                                 ),
-                                Item: ({ children, ...props }) => {
+                                Item: ({children, ...props}) => {
                                     return (
                                         <ListItem
                                             {...props}
@@ -114,7 +132,7 @@ export default function UserSearch({
                                     );
                                 },
                             }}
-                            data={users}
+                            data={listedUsers}
                             itemContent={(index, user) => (
                                 <ListItemButton
                                     onClick={() => {
