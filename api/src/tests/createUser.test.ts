7d8ac0e5ba4ace9594b29b03b47920f684a70ca0
Flo <floha2011@hotmail.de>
2025-01-04 05:09:11 +0100

    Create Basic Test Framework (#74)
    * basic test setup (not working)
    
    * tests run
    
    * github actions?
    
    * workflows should work
    
    * github actions should actually use integrated db
    
    * try to await server close and hope that will make tests exit gracefully
    
    * force exit to kill jest
    
    * remove n22 from ci

---
+++ api/src/tests/createUser.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { app } from '../main';
+import type http from 'http';
+
+let server: http.Server;
+
+beforeAll(() => {
+    // Start the server on a random port
+    server = app.listen(0);
+});
+
+afterAll( async () => {
+    // Close the server after tests
+    await server.close();
+});
+
+describe('Basic Test to create a new user', () => {
+    it('should create a new user when calling the corresponding route', async () => {
+        const res = await request(app)
+            .post('/api/registration/register')
+            .send({
+                email: 'test@gmail.com',
+                username: 'Test',
+                password: 'password',
+            })
+            .expect(201);
+        
+        expect(res.status).toBe(201);
+    });
+});
---
+++ api/src/tests/test.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'jest';
+
+const config : Config = {
+    testMatch: ['**/*.test.js'],
+    collectCoverage: true
+}
+
+export default config;
