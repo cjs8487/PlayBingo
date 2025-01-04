be2ee5fb2208756a92b5d36d856a6b4764f02758
cjs8487 <cjschaefer1999@gmail.com>
2025-01-04 14:53:50 -0600

    run next 15 codemod for newly awaitable refs

--- web/src/app/(main)/rooms/[slug]/layout.tsx
+++ web/src/app/(main)/rooms/[slug]/layout.tsx
@@ -1,13 +1,22 @@
 import { ReactNode } from 'react';
 import { RoomContextProvider } from '@/context/RoomContext';
 
-export default function RoomLayout({
-    params: { slug },
-    children,
-}: {
-    params: { slug: string };
-    children: ReactNode;
-}) {
+export default async function RoomLayout(
+    props: {
+        params: Promise<{ slug: string }>;
+        children: ReactNode;
+    }
+) {
+    const params = await props.params;
+
+    const {
+        slug
+    } = params;
+
+    const {
+        children
+    } = props;
+
     return (
         <>
             <RoomContextProvider slug={slug}>{children}</RoomContextProvider>
