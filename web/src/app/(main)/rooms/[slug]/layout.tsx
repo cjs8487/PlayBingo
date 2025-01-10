1c8e9231389e423545e9770f5d19f0f892845d17
cjs8487 <cjschaefer1999@gmail.com>
2025-01-09 22:58:58 -0600

    check room for 404 during server render

--- web/src/app/(main)/rooms/[slug]/layout.tsx
+++ web/src/app/(main)/rooms/[slug]/layout.tsx
@@ -1,25 +1,31 @@
 import { ReactNode } from 'react';
 import { RoomContextProvider } from '@/context/RoomContext';
+import { notFound } from 'next/navigation';
 
-export default async function RoomLayout(
-    props: {
-        params: Promise<{ slug: string }>;
-        children: ReactNode;
-    }
-) {
-    const params = await props.params;
-
-    const {
-        slug
-    } = params;
-
-    const {
-        children
-    } = props;
-
-    return (
-        <>
-            <RoomContextProvider slug={slug}>{children}</RoomContextProvider>
-        </>
+async function loadRoom(slug: string) {
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_PATH}/api/rooms/${slug}`,
     );
+
+    if (!res.ok) {
+        if (res.status === 404) {
+            return notFound();
+        }
+    }
+
+    return {};
+}
+
+export default async function RoomLayout({
+    params,
+    children,
+}: {
+    params: Promise<{ slug: string }>;
+    children: ReactNode;
+}) {
+    const { slug } = await params;
+
+    const roomData = await loadRoom(slug);
+
+    return <RoomContextProvider slug={slug}>{children}</RoomContextProvider>;
 }
