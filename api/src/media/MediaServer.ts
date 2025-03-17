82bf84dfb639bab1f6ff4e08ef10ef91913ef224
cjs8487 <cjschaefer1999@gmail.com>
2025-03-16 23:42:56 -0400

    save pending files

--- api/src/media/MediaServer.ts
+++ api/src/media/MediaServer.ts
@@ -1,8 +1,59 @@
 import { randomUUID } from 'crypto';
 import { Router } from 'express';
 import fileUpload, { UploadedFile } from 'express-fileupload';
+import path from 'path';
 
 const mediaServer = Router();
+
+interface PendingFileData {
+    file: UploadedFile;
+    workflow: string;
+    timeout: NodeJS.Timeout;
+}
+
+const pendingFiles: Record<string, PendingFileData> = {};
+
+const pendFile = (file: UploadedFile, workflow: string) => {
+    const id = randomUUID();
+    pendingFiles[id] = {
+        file,
+        workflow,
+        timeout: setTimeout(
+            () => {
+                delete pendingFiles[id];
+            },
+            5 * 60 * 1000,
+        ),
+    };
+    return id;
+};
+
+export const saveFile = async (id: string) => {
+    const pendingFile = pendingFiles[id];
+    if (!pendingFile) {
+        return false;
+    }
+
+    const { file, workflow, timeout } = pendingFile;
+
+    clearTimeout(timeout);
+
+    const success = await new Promise((resolve) => {
+        file.mv(
+            path.resolve('media', workflow, `${id}.${path.extname(file.name)}`),
+            (err) => {
+                if (err) {
+                    resolve(false);
+                } else {
+                    delete pendingFiles[id];
+                    resolve(true);
+                }
+            },
+        );
+    });
+
+    return success;
+};
 
 mediaServer.use(fileUpload());
 
@@ -11,7 +62,25 @@ mediaServer.post('/', (req, res) => {
         return res.status(400).send('No files were uploaded.');
     }
 
-    res.sendStatus(200);
+    const { workflow } = req.body;
+    if (!workflow) {
+        return res.status(400).send('Missing file workflow');
+    }
+
+    const ids: string[] = [];
+    Object.keys(req.files).forEach((k) => {
+        if (!req.files) return;
+
+        if (Array.isArray(req.files[k])) {
+            req.files[k].forEach((file) => {
+                ids.push(pendFile(file, workflow));
+            });
+        } else {
+            ids.push(pendFile(req.files[k], workflow));
+        }
+    });
+
+    res.status(200).send({ ids });
 });
 
 export default mediaServer;
