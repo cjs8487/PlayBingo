d4ce1456a4aa61d08582ade0ae1f2527ebe613e1
cjs8487 <cjschaefer1999@gmail.com>
2025-03-17 11:34:40 -0400

    set up games to use new media structure

--- web/src/app/(main)/games/[slug]/page.tsx
+++ web/src/app/(main)/games/[slug]/page.tsx
@@ -1,29 +1,24 @@
 'use client';
+
 import { useApi } from '@/lib/Hooks';
 import { Game } from '@/types/Game';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { Box, Container, Link, Tab, Typography } from '@mui/material';
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { useLayoutEffect, useState, use } from 'react';
+import { use, useLayoutEffect, useState } from 'react';
 import GameSettings from '../../../../components/game/GameSettings';
+import GoalCategories from '../../../../components/game/GoalCategories';
 import PermissionsManagement from '../../../../components/game/PermissionsManagement';
+import Variants from '../../../../components/game/Variants';
 import GoalManagement from '../../../../components/game/goals/GoalManagement';
 import { GoalManagerContextProvider } from '../../../../context/GoalManagerContext';
-import { alertError } from '../../../../lib/Utils';
-import Variants from '../../../../components/game/Variants';
-import GoalCategories from '../../../../components/game/GoalCategories';
+import { alertError, gameCoverUrl } from '../../../../lib/Utils';
 
-export default function GamePage(
-    props: {
-        params: Promise<{ slug: string }>;
-    }
-) {
+export default function GamePage(props: { params: Promise<{ slug: string }> }) {
     const params = use(props.params);
 
-    const {
-        slug
-    } = params;
+    const { slug } = params;
 
     const { data: gameData, isLoading } = useApi<Game>(`/api/games/${slug}`);
 
@@ -78,7 +73,11 @@ export default function GamePage(
             <Box display="flex">
                 <Box position="fixed" mr={4}>
                     {gameData.coverImage && (
-                        <Image src={gameData.coverImage} alt="" fill />
+                        <Image
+                            src={gameCoverUrl(gameData.coverImage)}
+                            alt=""
+                            fill
+                        />
                     )}
                     {!gameData.coverImage && (
                         <div>
