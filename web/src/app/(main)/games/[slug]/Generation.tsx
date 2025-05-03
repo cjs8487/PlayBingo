e072092ba4dccb7a9b8508a385d5c9f1091563b2
cjs8487 <cjschaefer1999@gmail.com>
2025-05-03 13:01:34 -0500

    setup form

--- web/src/app/(main)/games/[slug]/Generation.tsx
+++ web/src/app/(main)/games/[slug]/Generation.tsx
@@ -6,6 +6,7 @@ import {
     CardContent,
     CircularProgress,
     FormControl,
+    FormHelperText,
     InputLabel,
     ListItemText,
     MenuItem,
@@ -14,28 +15,47 @@ import {
     Typography,
 } from '@mui/material';
 import { GeneratorOptions, GeneratorStep } from '@playbingo/types';
-import { Form, Formik } from 'formik';
-import { useEffect, useState } from 'react';
+import { Form, Formik, useField, useFormik } from 'formik';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { FormikSelectField } from '../../../../components/input/FormikSelectField';
 
+type GeneratorConfigSingle = string;
+type GeneratorConfigMultiple = string[];
 interface StepProps {
     step: GeneratorStep;
 }
 
-function GenerationStep({
-    step: {
-        value,
-        displayName,
-        description,
-        required,
-        selectMultiple,
-        availableRules,
-    },
+function GenerationStepSingle({
+    step: { value: name, displayName, description, required, availableRules },
 }: StepProps) {
+    const [{}, meta] = useField<GeneratorConfigSingle>(name);
+
+    const options = useMemo(
+        () =>
+            availableRules.map((r) => ({
+                label: r.displayName,
+                value: r.value,
+                tooltip: r.description,
+            })),
+        [availableRules],
+    );
+
+    const error = meta.touched && !!meta.error;
+
     return (
-        <Card variant="outlined" component="fieldset" sx={{ mb: 1 }}>
+        <Card
+            variant="outlined"
+            component="fieldset"
+            sx={{
+                mb: 1,
+                borderColor: (theme) =>
+                    error ? theme.palette.error.main : theme.palette.divider,
+            }}
+        >
             <Typography
                 sx={{
                     px: 1,
+                    color: (theme) => (error ? theme.palette.error.main : ''),
                 }}
                 component="legend"
             >
@@ -45,25 +65,84 @@ function GenerationStep({
                 <Typography variant="body2" sx={{ mb: 4 }}>
                     {description}
                 </Typography>
-                {!selectMultiple && (
-                    <FormControl fullWidth>
-                        <InputLabel>Value</InputLabel>
-                        <Select label="Value">
-                            {availableRules.map((rule) => (
-                                <MenuItem key={rule.value} value={rule.value}>
-                                    <Tooltip title={rule.description}>
-                                        <ListItemText>
-                                            {rule.displayName}
-                                        </ListItemText>
-                                    </Tooltip>
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
+                <FormikSelectField
+                    id={`${name}`}
+                    name={`${name}`}
+                    label=""
+                    options={options}
+                    sx={{ width: '100%', mb: 2 }}
+                />
+            </CardContent>
+        </Card>
+    );
+}
+function GenerationStepMultiple({
+    step: { value: name, displayName, description, availableRules },
+}: StepProps) {
+    const [{ value }, meta, { setValue }] =
+        useField<GeneratorConfigMultiple>(name);
+
+    const addNewValue = useCallback(() => {
+        value.push('');
+        setValue(value);
+    }, [value, setValue]);
+
+    const options = useMemo(
+        () =>
+            availableRules.map((r) => ({
+                label: r.displayName,
+                value: r.value,
+                tooltip: r.description,
+            })),
+        [availableRules],
+    );
+
+    const error =
+        meta.touched &&
+        (Array.isArray(meta.error) ? meta.error.length > 0 : !!meta.error);
+
+    return (
+        <Card
+            variant="outlined"
+            component="fieldset"
+            sx={{
+                mb: 1,
+                borderColor: (theme) =>
+                    error ? theme.palette.error.main : theme.palette.divider,
+            }}
+        >
+            <Typography
+                sx={{
+                    px: 1,
+                    color: (theme) => (error ? theme.palette.error.main : ''),
+                }}
+                component="legend"
+            >
+                {displayName}
+            </Typography>
+            <CardContent>
+                <Typography variant="body2" sx={{ mb: 2 }}>
+                    {description}
+                </Typography>
+                {error && !Array.isArray(meta.error) && (
+                    <FormHelperText error={error}>{meta.error}</FormHelperText>
                 )}
-                {selectMultiple && (
+                {value.map((v, idx) => (
+                    <FormikSelectField
+                        id={`${name}-${idx}`}
+                        name={`${name}[${idx}]`}
+                        label=""
+                        options={availableRules.map((r) => ({
+                            label: r.displayName,
+                            value: r.value,
+                            tooltip: r.description,
+                        }))}
+                        sx={{ width: '100%', mb: 2 }}
+                    />
+                ))}
+                {availableRules.length > 0 && (
                     <Card variant="outlined" sx={{ borderStyle: 'dashed' }}>
-                        <CardActionArea>
+                        <CardActionArea onClick={addNewValue}>
                             <CardContent sx={{ textAlign: 'center' }}>
                                 <Add />
                                 <Typography>Add new rule</Typography>
@@ -108,17 +187,58 @@ export default function GenerationPage() {
 
     const { steps } = generatorOptions;
 
+    const initialValues: { [k: string]: string | string[] } = {};
+    generatorOptions.steps.forEach((s) => {
+        if (s.selectMultiple) {
+            initialValues[s.value] = [];
+        } else {
+            initialValues[s.value] = '';
+        }
+    });
+
     return (
         <Formik
-            initialValues={{}}
+            initialValues={initialValues}
             onSubmit={(values) => {
                 console.log(values);
             }}
+            validate={(values) => {
+                const errors: { [k: string]: string | string[] } = {};
+                steps.forEach((s) => {
+                    if (s.required) {
+                        if (
+                            (s.selectMultiple &&
+                                values[s.value].length === 0) ||
+                            (!s.selectMultiple && !values[s.value])
+                        ) {
+                            errors[s.value] = `${s.displayName} is required.`;
+                        }
+                    }
+
+                    if (s.selectMultiple) {
+                        const valList = values[s.value] as string[];
+                        if (valList.length > 0) {
+                            errors[s.value] = [];
+                            valList.forEach((v, idx) => {
+                                if (valList.indexOf(v) !== idx) {
+                                    (errors[s.value] as string[])[idx] =
+                                        'Duplicate values are not allowed.';
+                                }
+                            });
+                        }
+                    }
+                });
+                return errors;
+            }}
         >
             <Form>
-                {steps.map((step) => (
-                    <GenerationStep key={step.value} step={step} />
-                ))}
+                {steps.map((step) =>
+                    step.selectMultiple ? (
+                        <GenerationStepMultiple key={step.value} step={step} />
+                    ) : (
+                        <GenerationStepSingle key={step.value} step={step} />
+                    ),
+                )}
                 <Button type="submit">Save</Button>
             </Form>
         </Formik>
