ab490562b3cac1324b733cd7369f15606114c52c
cjs8487 <cjschaefer1999@gmail.com>
2025-05-12 18:22:34 -0500

    re-add difficulty to non-empty body check for goals

--- api/src/routes/goals/Goals.ts
+++ api/src/routes/goals/Goals.ts
@@ -33,7 +33,7 @@ goals.post('/:id', async (req, res) => {
     const { id } = req.params;
     const { goal, description, categories, difficulty } = req.body;
 
-    if (!goal && description === undefined && !categories) {
+    if (!goal && description === undefined && !categories && !difficulty) {
         res.status(400).send('No changes submitted');
         return;
     }
