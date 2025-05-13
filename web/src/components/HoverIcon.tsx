175e9fbe023dd3ca54d2190ba6d788bbb51937af
cjs8487 <cjschaefer1999@gmail.com>
2025-05-12 20:05:17 -0500

    switch to mui tooltip in hover icon

--- web/src/components/HoverIcon.tsx
+++ web/src/components/HoverIcon.tsx
@@ -1,35 +1,12 @@
 import { useFloating, useHover, useInteractions } from '@floating-ui/react';
-import { Paper } from '@mui/material';
-import { ReactNode, useState } from 'react';
+import { Paper, Tooltip } from '@mui/material';
+import { ReactElement, ReactNode, useState } from 'react';
 
 interface HoverIconProps {
-    icon: ReactNode;
+    icon: ReactElement;
     children: ReactNode;
 }
 
 export default function HoverIcon({ icon, children }: HoverIconProps) {
-    const [open, setOpen] = useState(false);
-    const { refs, floatingStyles, context } = useFloating({
-        open: open,
-        onOpenChange: setOpen,
-    });
-    const hover = useHover(context);
-    const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
-    return (
-        <>
-            <div ref={refs.setReference} {...getReferenceProps()}>
-                {icon}
-            </div>
-            {open && (
-                <Paper
-                    ref={refs.setFloating}
-                    style={floatingStyles}
-                    {...getFloatingProps()}
-                    sx={{ p: 2, maxWidth: 'sm', zIndex: 10 }}
-                >
-                    {children}
-                </Paper>
-            )}
-        </>
-    );
+    return <Tooltip title={children}>{icon}</Tooltip>;
 }
