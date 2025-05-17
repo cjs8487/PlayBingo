9231d3cf40cc61a0f159a4ec3ffea95a7d24de13
Floha <floha2011@hotmail.de>
2025-05-17 20:23:55 +0200

    improve error to be 1 indexed and floor rows

--- api/src/core/generation/GenerationFailedError.ts
+++ api/src/core/generation/GenerationFailedError.ts
@@ -16,7 +16,7 @@ export class GenerationFailedError extends Error {
 
         let message = `Failed to generate board - ${failureReason}. `;
         if (boardIndex >= 0) {
-            message += `This error occurred while attempting to place a goal at row ${boardIndex / 5}, column ${boardIndex % 5}.`;
+            message += `This error occurred while attempting to place a goal at row ${Math.floor(boardIndex / 5) + 1}, column ${(boardIndex % 5) + 1}.`;
         } else {
             message += 'This error occurred prior to placing the first goal.';
         }
