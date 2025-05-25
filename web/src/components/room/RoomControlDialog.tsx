694081226c1c0ce30f46e3f4a54a1907f4e10486
cjs8487 <cjschaefer1999@gmail.com>
2025-05-24 22:47:00 -0500

    fix toggle label

--- web/src/components/room/RoomControlDialog.tsx
+++ web/src/components/room/RoomControlDialog.tsx
@@ -164,7 +164,7 @@ export default function RoomControlDialog({
                                 onChange={toggleCounters}
                             />
                         }
-                        label="Show All Goal Details"
+                        label="Show Counters"
                     />
                 </Box>
                 <Box
