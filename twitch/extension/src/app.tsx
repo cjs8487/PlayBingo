057c16ff5c361432c72c7b5059ba86205f60501f
cjs8487 <cjschaefer1999@gmail.com>
2025-05-27 18:16:22 -0500

    set up basic board rendering scheme

--- twitch/extension/src/app.tsx
+++ twitch/extension/src/app.tsx
@@ -1,43 +1,46 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+const board = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+];
 
 export function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
+    console.log(board);
+    return (
+        <div
+            style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: 'purple',
+            }}
         >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+            {board.map((row, x) => (
+                <div
+                    style={{
+                        width: '100%',
+                        flexGrow: 1,
+                        display: 'flex',
+                        backgroundColor: 'green',
+                        borderCollapse: 'collapse',
+                    }}
+                >
+                    {row.map((cell, y) => (
+                        <div
+                            style={{
+                                flexGrow: 1,
+                                border: '1px solid black',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >{`${x},${y}`}</div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
 }
