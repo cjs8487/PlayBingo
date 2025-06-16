aa4915c9f56db0a9693a752964aadf3fe8c4f2c7
cjs8487 <cjschaefer1999@gmail.com>
2025-06-16 17:58:58 -0500

    add mod and spectator indicator

--- web/src/components/room/PlayerInfo.tsx
+++ web/src/components/room/PlayerInfo.tsx
@@ -1,23 +1,27 @@
 import { Card, CardContent, Box, Typography, Button } from '@mui/material';
 import { ConnectionStatus, useRoomContext } from '../../context/RoomContext';
 import ColorSelect from './ColorSelect';
+import { Sword } from 'mdi-material-ui';
 
 export default function PlayerInfo() {
-    const { connectionStatus, nickname, disconnect } = useRoomContext();
+    const { connectionStatus, nickname, disconnect, monitor, spectator } =
+        useRoomContext();
     return (
         <Card>
             <CardContent>
                 <Box
                     sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        flexGrow: 1
-                    }}>
-                    <Typography variant="h6" sx={{
-                        flexGrow: 1
-                    }}>
-                        Playing as {nickname}
+                        display: 'flex',
+                        alignItems: 'center',
+                        flexGrow: 1,
+                        columnGap: 1,
+                    }}
+                >
+                    <Typography variant="h6">
+                        {spectator ? 'Spectating' : 'Playing'} as {nickname}
                     </Typography>
+                    {monitor && <Sword sx={{ color: 'green' }} />}
+                    <Box sx={{ flexGrow: 1 }} />
                     {connectionStatus !== ConnectionStatus.CLOSED && (
                         <Button onClick={disconnect}>Disconnect</Button>
                     )}
