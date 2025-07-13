d987dde102956b309a689e10656c6e444c9f97c9
cjs8487 <cjschaefer1999@gmail.com>
2025-07-12 21:48:59 -0500

    show description/setup

--- web/src/app/(main)/games/[slug]/Summary.tsx
+++ web/src/app/(main)/games/[slug]/Summary.tsx
@@ -1,6 +1,7 @@
 import { Box, Card, CardContent, Typography } from '@mui/material';
 import { Game } from '@playbingo/types';
 import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
 interface Props {
     gameData: Game;
@@ -15,34 +16,33 @@ export default function Summary({ gameData }: Props) {
                 gap: 2,
             }}
         >
-            <Card sx={{ gridRow: '1' }}>
-                <CardContent>
-                    <Typography variant="h5" sx={{ mb: 1 }}>
-                        {gameData.name}
-                    </Typography>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
-                    </Typography>
-                    {(gameData.difficultyVariants?.length ?? 0) > 0 && (
-                        <Box sx={{ mt: 2 }}>
-                            <Typography variant="h6">Variants</Typography>
-                            {gameData.difficultyVariants?.map((variant) => (
-                                <Typography key={variant.id}>
-                                    {variant.name}
+            {(gameData.descriptionMd ||
+                (gameData.difficultyVariants?.length ?? 0) > 0) && (
+                <Card sx={{ gridRow: '1' }}>
+                    <CardContent>
+                        {gameData.descriptionMd && (
+                            <>
+                                <Typography variant="h5" sx={{ mb: 1 }}>
+                                    {gameData.name}
                                 </Typography>
-                            ))}
-                        </Box>
-                    )}
-                </CardContent>
-            </Card>
+                                <ReactMarkdown>
+                                    {gameData.descriptionMd}
+                                </ReactMarkdown>
+                            </>
+                        )}
+                        {(gameData.difficultyVariants?.length ?? 0) > 0 && (
+                            <Box sx={{ mt: 2 }}>
+                                <Typography variant="h6">Variants</Typography>
+                                {gameData.difficultyVariants?.map((variant) => (
+                                    <Typography key={variant.id}>
+                                        {variant.name}
+                                    </Typography>
+                                ))}
+                            </Box>
+                        )}
+                    </CardContent>
+                </Card>
+            )}
             <Card sx={{ gridRow: '1/-1' }}>
                 <CardContent>
                     <Typography variant="h5">Links</Typography>
@@ -51,22 +51,14 @@ export default function Summary({ gameData }: Props) {
                     </Typography>
                 </CardContent>
             </Card>
-            <Card>
-                <CardContent>
-                    <Typography variant="h5">Setup</Typography>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
-                    </Typography>
-                </CardContent>
-            </Card>
+            {gameData.setupMd && (
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5">Setup</Typography>
+                        <ReactMarkdown>{gameData.setupMd}</ReactMarkdown>
+                    </CardContent>
+                </Card>
+            )}
         </Box>
     );
 }
