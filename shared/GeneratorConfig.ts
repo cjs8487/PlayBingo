7446a01bf066dfb9dad43edd7968263c3a92f9c0
cjs8487 <cjschaefer1999@gmail.com>
2025-09-19 20:39:38 -0700

    inject categories as enum at runtime

--- shared/GeneratorConfig.ts
+++ shared/GeneratorConfig.ts
@@ -1,67 +1,69 @@
-import { z } from 'zod';
+import * as z from 'zod';
+import { GoalCategory } from '@playbingo/types';
 
-export const GenerationListModeSchema = z.discriminatedUnion('mode', [
-    z.object({
-        mode: z.literal('all'), // default, keep all goals
-    }),
-    z.object({
-        mode: z.literal('difficulty-filter'),
-        min: z.number().int().min(1).optional(),
-        max: z.number().int().min(1).optional(),
-    }),
-    z.object({
-        mode: z.literal('category-filter'),
-        include: z.array(z.string()).optional(),
-        exclude: z.array(z.string()).optional(),
-    }),
-]);
-export type GenerationListMode = z.infer<typeof GenerationListModeSchema>;
+export const makeGeneratorSchema = (categories: GoalCategory[]) => {
+    const enumLabels: Record<string, string> = {};
 
-export const GenerationListTransformSchema = z.enum(['none']);
-export type GenerationListTransform = z.infer<
-    typeof GenerationListTransformSchema
+    const catIds = categories.map((c) => {
+        enumLabels[c.id] = c.name;
+        return c.id;
+    });
+
+    const GenerationListModeSchema = z.discriminatedUnion('mode', [
+        z.object({
+            mode: z.literal('all'), // default, keep all goals
+        }),
+        z.object({
+            mode: z.literal('difficulty-filter'),
+            min: z.number().int().min(1).optional(),
+            max: z.number().int().min(1).optional(),
+        }),
+        z.object({
+            mode: z.literal('category-filter'),
+            include: z.array(z.enum(catIds)).optional(),
+            exclude: z.array(z.enum(catIds)).optional(),
+        }),
+    ]);
+
+    const GenerationListTransformSchema = z.enum(['none']);
+
+    const GenerationBoardLayoutSchema = z.enum(['none', 'srlv5', 'isaac']);
+
+    const GenerationGoalSelectionSchema = z.enum(['random', 'difficulty']);
+
+    const GenerationGoalRestrictionSchema = z.discriminatedUnion('type', [
+        z.object({
+            type: z.literal('line-type-exclusion'),
+        }),
+        z.object({
+            type: z.literal('category-cap'),
+            category: z.string(),
+            max: z.number().int().min(1),
+        }),
+    ]);
+
+    const GenerationGlobalAdjustmentsSchema = z.discriminatedUnion('type', [
+        z.object({
+            type: z.literal('synergize'),
+        }),
+        z.object({
+            type: z.literal('board-type-max'),
+        }),
+    ]);
+
+    return {
+        schema: z.object({
+            listMode: GenerationListModeSchema.default({ mode: 'all' }),
+            listTransform: GenerationListTransformSchema.default('none'),
+            boardLayout: GenerationBoardLayoutSchema.default('none'),
+            goalSelection: GenerationGoalSelectionSchema.default('random'),
+            restrictions: z.array(GenerationGoalRestrictionSchema).default([]),
+            adjustments: z.array(GenerationGlobalAdjustmentsSchema).default([]),
+        }),
+        enumLabels,
+    };
+};
+
+export type GeneratorConfig = z.infer<
+    ReturnType<typeof makeGeneratorSchema>['schema']
 >;
-
-export const GenerationBoardLayoutSchema = z.enum(['none', 'srlv5', 'isaac']);
-export type GenerationBoardLayout = z.infer<typeof GenerationBoardLayoutSchema>;
-
-export const GenerationGoalSelectionSchema = z.enum(['random', 'difficulty']);
-export type GenerationGoalSelection = z.infer<
-    typeof GenerationGoalSelectionSchema
->;
-
-export const GenerationGoalRestrictionSchema = z.discriminatedUnion('type', [
-    z.object({
-        type: z.literal('line-type-exclusion'),
-    }),
-    z.object({
-        type: z.literal('category-cap'),
-        category: z.string(),
-        max: z.number().int().min(1),
-    }),
-]);
-export type GenerationGoalRestriction = z.infer<
-    typeof GenerationGoalRestrictionSchema
->;
-
-export const GenerationGlobalAdjustmentsSchema = z.discriminatedUnion('type', [
-    z.object({
-        type: z.literal('synergize'),
-    }),
-    z.object({
-        type: z.literal('board-type-max'),
-    }),
-]);
-export type GenerationGlobalAdjustment = z.infer<
-    typeof GenerationGlobalAdjustmentsSchema
->;
-
-export const GeneratorConfigSchema = z.object({
-    listMode: GenerationListModeSchema.default({ mode: 'all' }),
-    listTransform: GenerationListTransformSchema.default('none'),
-    boardLayout: GenerationBoardLayoutSchema.default('none'),
-    goalSelection: GenerationGoalSelectionSchema.default('random'),
-    restrictions: z.array(GenerationGoalRestrictionSchema).default([]),
-    adjustments: z.array(GenerationGlobalAdjustmentsSchema).default([]),
-});
-export type GeneratorConfig = z.infer<typeof GeneratorConfigSchema>;
--- web/src/app/(main)/games/[slug]/generation/_components/GenerationForm.tsx
+++ web/src/app/(main)/games/[slug]/generation/_components/GenerationForm.tsx
@@ -1,195 +1,24 @@
 'use client';
-import { FormikSelectField } from '@/components/input/FormikSelectField';
 import {
     JSONSchema,
     JsonSchemaRenderer,
     JSONValue,
 } from '@/components/input/JsonSchemaRenderer';
-import { alertError, notifyMessage } from '@/lib/Utils';
-import { Add, Delete } from '@mui/icons-material';
-import {
-    Box,
-    Button,
-    Card,
-    CardActionArea,
-    CardContent,
-    FormHelperText,
-    IconButton,
-    Typography,
-} from '@mui/material';
-import { GeneratorConfigSchema } from '@playbingo/shared/GeneratorConfig';
-import { Game, GeneratorSettings, GeneratorStep } from '@playbingo/types';
-import { Form, Formik, useField } from 'formik';
-import { useCallback, useMemo, useState } from 'react';
+import { Box, Button } from '@mui/material';
+import { makeGeneratorSchema } from '@playbingo/shared/GeneratorConfig';
+import { GoalCategory } from '@playbingo/types';
+import { useState } from 'react';
 import * as z from 'zod';
 
-type GeneratorConfigSingle = string;
-type GeneratorConfigMultiple = string[];
-interface StepProps {
-    step: GeneratorStep;
-}
-
-function GenerationStepSingle({
-    step: { value: name, displayName, description, availableRules },
-}: StepProps) {
-    const [, meta] = useField<GeneratorConfigSingle>(name);
-
-    const options = useMemo(
-        () =>
-            availableRules.map((r) => ({
-                label: r.displayName,
-                value: r.value,
-                tooltip: r.description,
-            })),
-        [availableRules],
-    );
-
-    const error = meta.touched && !!meta.error;
-
-    return (
-        <Card
-            variant="outlined"
-            component="fieldset"
-            sx={{
-                mb: 1,
-                borderColor: error ? 'error.main' : 'divider',
-                background: 'unset',
-            }}
-        >
-            <Typography
-                sx={{
-                    px: 1,
-                    color: error ? 'error.main' : '',
-                }}
-                component="legend"
-            >
-                {displayName}
-            </Typography>
-            <CardContent>
-                <Typography variant="body2">{description}</Typography>
-                {availableRules.length > 0 && (
-                    <FormikSelectField
-                        id={`${name}`}
-                        name={`${name}`}
-                        label=""
-                        options={options}
-                        sx={{ width: '100%', mt: 2 }}
-                    />
-                )}
-            </CardContent>
-        </Card>
-    );
-}
-function GenerationStepMultiple({
-    step: { value: name, displayName, description, availableRules },
-}: StepProps) {
-    const [{ value }, meta, { setValue }] =
-        useField<GeneratorConfigMultiple>(name);
-
-    const addNewValue = useCallback(() => {
-        value.push('');
-        setValue(value);
-    }, [value, setValue]);
-
-    const removeValue = useCallback(
-        (index: number) => {
-            value.splice(index, 1);
-            setValue(value);
-        },
-        [value, setValue],
-    );
-
-    const options = useMemo(
-        () =>
-            availableRules.map((r) => ({
-                label: r.displayName,
-                value: r.value,
-                tooltip: r.description,
-            })),
-        [availableRules],
-    );
-
-    const error =
-        meta.touched &&
-        (Array.isArray(meta.error) ? meta.error.length > 0 : !!meta.error);
-
-    return (
-        <Card
-            variant="outlined"
-            component="fieldset"
-            sx={{
-                mb: 1,
-                borderColor: error ? 'error.main' : 'divider',
-                background: 'unset',
-            }}
-        >
-            <Typography
-                sx={{
-                    px: 1,
-                    color: error ? 'error.main' : '',
-                }}
-                component="legend"
-            >
-                {displayName}
-            </Typography>
-            <CardContent>
-                <Typography variant="body2">{description}</Typography>
-                {error && !Array.isArray(meta.error) && (
-                    <FormHelperText error={error}>{meta.error}</FormHelperText>
-                )}
-                {value.map((_, idx) => (
-                    <Box
-                        key={idx}
-                        sx={{
-                            display: 'flex',
-                            columnGap: 0.5,
-                            mt: 2,
-                        }}
-                    >
-                        <FormikSelectField
-                            id={`${name}-${idx}`}
-                            name={`${name}[${idx}]`}
-                            label=""
-                            options={options}
-                            sx={{ flexGrow: 1 }}
-                        />
-                        <IconButton onClick={() => removeValue(idx)}>
-                            <Delete />
-                        </IconButton>
-                    </Box>
-                ))}
-                {availableRules.length > 0 && (
-                    <Card
-                        variant="outlined"
-                        sx={{
-                            borderStyle: 'dashed',
-                            mt: 2,
-                            background: 'unset',
-                        }}
-                    >
-                        <CardActionArea onClick={addNewValue}>
-                            <CardContent sx={{ textAlign: 'center' }}>
-                                <Add />
-                                <Typography>Add new rule</Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                )}
-            </CardContent>
-        </Card>
-    );
-}
-
 interface Props {
-    game: Game;
+    categories: GoalCategory[];
 }
 
-const schemaJson = z.toJSONSchema(GeneratorConfigSchema);
+export default function GenerationPage({ categories }: Props) {
+    const { schema, enumLabels } = makeGeneratorSchema(categories);
+    const schemaJson = z.toJSONSchema(schema);
 
-export default function GenerationPage({ game }: Props) {
     const [config, setConfig] = useState<JSONValue>();
-
-    console.log(schemaJson);
 
     return (
         <Box sx={{ width: '100%' }}>
@@ -212,6 +41,7 @@ export default function GenerationPage({ game }: Props) {
                     all: 'All Goals',
                     'difficulty-filter': 'Difficulty Filter',
                     'category-filter': 'Category Filter',
+                    ...enumLabels,
                 }}
             />
             <pre>{JSON.stringify(config, null, 2)}</pre>
@@ -224,82 +54,5 @@ export default function GenerationPage({ game }: Props) {
                 </Button>
             </Box>
         </Box>
-    );
-
-    return (
-        <Formik
-            initialValues={game.generationSettings}
-            onSubmit={async (values) => {
-                const res = await fetch(`/api/games/${game.slug}/generation`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(values),
-                });
-                if (!res.ok) {
-                    alertError(
-                        `Failed to save generator options - ${await res.text()}`,
-                    );
-                    return;
-                }
-
-                notifyMessage('Successfully updated generation settings');
-            }}
-            validate={(values) => {
-                const errors: { [k: string]: string | string[] } = {};
-                steps.forEach((s) => {
-                    if (s.required) {
-                        if (
-                            (s.selectMultiple &&
-                                values[s.value as keyof GeneratorSettings]
-                                    .length === 0) ||
-                            (!s.selectMultiple &&
-                                !values[s.value as keyof GeneratorSettings])
-                        ) {
-                            errors[s.value] = `${s.displayName} is required.`;
-                        }
-                    }
-
-                    if (s.selectMultiple) {
-                        const valList = values[
-                            s.value as keyof GeneratorSettings
-                        ] as string[];
-                        if (valList.length > 0) {
-                            errors[s.value] = [];
-                            valList.forEach((v, idx) => {
-                                if (valList.indexOf(v) !== idx) {
-                                    (errors[s.value] as string[])[idx] =
-                                        'Duplicate values are not allowed.';
-                                }
-                            });
-
-                            if (errors[s.value].length === 0) {
-                                delete errors[s.value];
-                            }
-                        }
-                    }
-                });
-                return errors;
-            }}
-        >
-            {({ resetForm }) => (
-                <Form>
-                    {steps.map((step) =>
-                        step.selectMultiple ? (
-                            <GenerationStepMultiple
-                                key={step.value}
-                                step={step}
-                            />
-                        ) : (
-                            <GenerationStepSingle
-                                key={step.value}
-                                step={step}
-                            />
-                        ),
-                    )}
-                </Form>
-            )}
-        </Formik>
     );
 }
