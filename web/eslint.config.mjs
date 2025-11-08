ffd029b25bd37f9f997d8c0365d74e7af832c4f8
cjs8487 <cjschaefer1999@gmail.com>
2025-11-07 22:20:13 -0600

    run codemod for eslint cli

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -3,26 +3,22 @@ import js from '@eslint/js';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 
 const eslintConfig = defineConfig([
-    // baseline JS recommended rules
     js.configs.recommended,
-
-    // compatibility layer for old-style configs
-
     eslintPluginPrettierRecommended,
     ...nextVitals,
     ...nextTs,
-
-    globalIgnores([
-        // Default ignores of eslint-config-next:
-        '.next/**',
-        'out/**',
-        'build/**',
-        'next-env.d.ts',
-        'node_modules/**',
-    ]),
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts',
+        ],
+    },
 ]);
 
 export default eslintConfig;
