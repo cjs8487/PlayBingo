8a0a5592cd4b6074656a8b87710912fcbc045b08
cjs8487 <cjschaefer1999@gmail.com>
2025-11-21 22:10:24 -0600

    update schemas

--- schema/index.d.ts
+++ schema/index.d.ts
@@ -5,6 +5,8 @@ export * from './types/Game';
 export * from './types/GeneratorOptions';
 export * from './types/Goal';
 export * from './types/GoalCategory';
+export * from './types/GoalImage';
+export * from './types/GoalImageTag';
 export * from './types/Player';
 export * from './types/RoomAction';
 export * from './types/RoomData';
