45770650a17d8c6fab36612fd5242ff44c981364
cjs8487 <cjschaefer1999@gmail.com>
2025-11-29 13:15:01 -0600

    make chips slightly translucent in rooms

--- web/src/components/board/Cell.tsx
+++ web/src/components/board/Cell.tsx
@@ -270,6 +270,7 @@ export default function BoardCell({
                                         mt: 0.5,
                                         mr: 0.5,
                                         backgroundColor: goal.imageTag.color,
+                                        opacity: 0.8,
                                     }}
                                 />
                             )}
