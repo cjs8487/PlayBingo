b1b1dee887e8f698333574d57448e724c7718301
cjs8487 <cjschaefer1999@gmail.com>
2025-12-15 22:27:10 -0600

    show player finish times regardless of provider

--- api/src/core/Room.ts
+++ api/src/core/Room.ts
@@ -844,7 +844,7 @@ export default class Room {
                         !player.goalComplete
                     ) {
                         player.goalComplete = true;
-                        this.raceHandler?.playerFinished(player);
+                        this.raceHandler?.playerFinished(player).then();
                         this.sendChat([
                             {
                                 contents: player.nickname,
