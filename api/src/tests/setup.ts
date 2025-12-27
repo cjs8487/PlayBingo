690b4f0a6e6c8379a38de4cc29c2aa7bfd974a3d
cjs8487 <cjschaefer1999@gmail.com>
2025-12-27 16:37:39 -0500

    add tests for join actions

--- api/src/tests/setup.ts
+++ api/src/tests/setup.ts
@@ -17,3 +17,24 @@ export const mockFindToken = jest
     .spyOn(prisma.apiToken, 'findUnique')
     .mockResolvedValueOnce(revokedTokenPayload)
     .mockResolvedValue(mockValidTokenPayload);
+
+export const mockCreateRoomAction = jest
+    .spyOn(prisma.roomAction, 'create')
+    .mockResolvedValue({
+        id: 'test',
+        action: 'JOIN',
+        payload: 'test',
+        roomId: 'test',
+    });
+export const mockPlayerUpsert = jest
+    .spyOn(prisma.player, 'upsert')
+    .mockResolvedValue({
+        id: 'test',
+        key: 'test',
+        nickname: 'test',
+        color: 'test',
+        roomId: 'test',
+        userId: null,
+        spectator: false,
+        monitor: false,
+    });
