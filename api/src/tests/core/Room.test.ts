adddce36c54086e817b06d7949e5a1d1f831e542
cjs8487 <cjschaefer1999@gmail.com>
2025-12-27 23:04:24 -0600

    add tests for chat, mark, and unmark actions

--- api/src/tests/core/Room.test.ts
+++ api/src/tests/core/Room.test.ts
@@ -1,30 +1,51 @@
-import { mock, mockReset } from 'jest-mock-extended';
-import Room from '../../core/Room';
-import { JoinAction, LeaveAction, RevealedCell } from '@playbingo/types';
-import { RoomTokenPayload } from '../../auth/RoomAuth';
+import {
+    ChatAction,
+    JoinAction,
+    LeaveAction,
+    MarkAction,
+    RevealedCell,
+    ServerMessage,
+    UnmarkAction,
+} from '@playbingo/types';
+import { mockDeep, mockReset } from 'jest-mock-extended';
 import WebSocket from 'ws';
-import { mockCreateRoomAction, mockPlayerUpsert } from '../setup';
+import { RoomTokenPayload } from '../../auth/RoomAuth';
 import Player from '../../core/Player';
-import e from 'cors';
+import Room from '../../core/Room';
+import { mockCreateRoomAction, mockPlayerUpsert } from '../setup';
 
 let room: Room;
 
-const mockJoinAction = mock<JoinAction>();
-const mockLeaveAction = mock<LeaveAction>();
+const mockJoinAction = mockDeep<JoinAction>();
+const mockLeaveAction = mockDeep<LeaveAction>();
+const mockChatAction = mockDeep<ChatAction>();
+mockChatAction.payload = {
+    message: 'test message',
+};
+const mockMarkAction = mockDeep<MarkAction>();
+mockMarkAction.payload = {
+    row: 3,
+    col: 2,
+};
+const mockUnmarkAction = mockDeep<UnmarkAction>();
+mockUnmarkAction.payload = {
+    row: 3,
+    col: 2,
+};
 
-const mockTokenPayload = mock<RoomTokenPayload>();
+const mockTokenPayload = mockDeep<RoomTokenPayload>();
 mockTokenPayload.playerId = 'test';
 mockTokenPayload.isSpectating = false;
-const mockTokenPayload2 = mock<RoomTokenPayload>();
+const mockTokenPayload2 = mockDeep<RoomTokenPayload>();
 mockTokenPayload2.playerId = 'test';
-const mockTokenPayloadPlayer2 = mock<RoomTokenPayload>();
+const mockTokenPayloadPlayer2 = mockDeep<RoomTokenPayload>();
 mockTokenPayloadPlayer2.playerId = 'test2';
 mockTokenPayloadPlayer2.isSpectating = false;
-const mockTokenPayloadSpectator = mock<RoomTokenPayload>();
+const mockTokenPayloadSpectator = mockDeep<RoomTokenPayload>();
 mockTokenPayloadSpectator.playerId = 'spectator';
 mockTokenPayloadSpectator.isSpectating = true;
-const mockSocket = mock<WebSocket>();
-const mockSocket2 = mock<WebSocket>();
+const mockSocket = mockDeep<WebSocket>();
+const mockSocket2 = mockDeep<WebSocket>();
 
 beforeEach(() => {
     room = new Room(
@@ -40,7 +61,7 @@ beforeEach(() => {
         false,
         'Normal',
     );
-    room.board = mock<RevealedCell[][]>();
+    room.board = mockDeep<RevealedCell[][]>();
 
     mockReset(mockJoinAction);
     mockReset(mockTokenPayload);
@@ -172,4 +193,188 @@ describe('handleLeave', () => {
     });
     // TODO: POST REFACTOR CHECK FOR UNAUTHORIZED RESPONSE SINCE THE RETURN TYPE
     // OF ACTION HANDLERS WILL LIKELY CHANGE
+});
+
+describe('handleChat', () => {
+    it('Sends a chat message to all players', () => {
+        const player = new Player(
+            room,
+            'test',
+            'Test Player',
+            'blue',
+            false,
+            false,
+        );
+        room.players.set(mockTokenPayload.playerId, player);
+        const sendChatSpy = jest.spyOn(room, 'sendChat');
+        room.handleChat(mockChatAction, mockTokenPayload);
+        expect(sendChatSpy).toHaveBeenCalledTimes(1);
+        expect(sendChatSpy).toHaveBeenCalledWith(
+            `${player.nickname}: test message`,
+        );
+        expect(mockCreateRoomAction).toHaveBeenCalledTimes(1);
+    });
+    // TODO: TEST UNAUTHORIZED
+});
+
+describe('Board Control', () => {
+    beforeEach(() => {
+        const player = new Player(
+            room,
+            'test',
+            'Test Player',
+            'blue',
+            false,
+            false,
+        );
+        player.addConnection(mockTokenPayload.uuid, mockSocket);
+        room.players.set(mockTokenPayload.playerId, player);
+
+        const player2 = new Player(
+            room,
+            'test2',
+            'Test Player',
+            'blue',
+            false,
+            false,
+        );
+        player2.addConnection(mockTokenPayloadPlayer2.uuid, mockSocket2);
+        room.players.set(mockTokenPayloadPlayer2.playerId, player2);
+
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                room.board[i][j].completedPlayers = [];
+            }
+        }
+    });
+
+    describe('Marking', () => {
+        it('Marks the correct cell if unmarked', () => {
+            room.handleMark(mockMarkAction, mockTokenPayload);
+            let completedPlayers =
+                room.board[mockMarkAction.payload.row][
+                    mockMarkAction.payload.col
+                ].completedPlayers;
+            expect(completedPlayers.length).toBe(1);
+            expect(completedPlayers).toContain(mockTokenPayload.playerId);
+            room.handleMark(mockMarkAction, mockTokenPayloadPlayer2);
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    const cell = room.board[i][j];
+                    if (
+                        i === mockMarkAction.payload.row &&
+                        j === mockMarkAction.payload.col
+                    ) {
+                        expect(cell.completedPlayers.length).toBe(2);
+                        expect(cell.completedPlayers).toContain(
+                            mockTokenPayload.playerId,
+                        );
+                        expect(cell.completedPlayers).toContain(
+                            mockTokenPayloadPlayer2.playerId,
+                        );
+                    } else {
+                        expect(cell.completedPlayers.length).toBe(0);
+                    }
+                }
+            }
+        });
+
+        it('Sends a cell message update', () => {
+            const playerSendSpy = jest.spyOn(
+                room.players.get(mockTokenPayload.playerId)!,
+                'sendMessage',
+            );
+            const player2SendSpy = jest.spyOn(
+                room.players.get(mockTokenPayloadPlayer2.playerId)!,
+                'sendMessage',
+            );
+            room.handleMark(mockMarkAction, mockTokenPayload);
+            const cellUpdateMessage: ServerMessage = {
+                action: 'cellUpdate',
+                row: mockMarkAction.payload.row,
+                col: mockMarkAction.payload.col,
+                cell: room.board[mockMarkAction.payload.row][
+                    mockMarkAction.payload.col
+                ],
+                players: room.getPlayers(),
+            };
+            expect(playerSendSpy).toHaveBeenCalledWith(cellUpdateMessage);
+            expect(player2SendSpy).toHaveBeenCalledWith(cellUpdateMessage);
+        });
+
+        it('Sends a chat message', () => {
+            const sendChatSpy = jest.spyOn(room, 'sendChat');
+            const player = room.players.get(mockTokenPayload.playerId)!;
+            const { row, col } = mockMarkAction.payload;
+            room.handleMark(mockMarkAction, mockTokenPayload);
+            expect(sendChatSpy).toHaveBeenCalledTimes(1);
+            expect(sendChatSpy).toHaveBeenCalledWith([
+                {
+                    contents: player.nickname,
+                    color: player.color,
+                },
+                ` marked ${room.board[row][col].goal.goal} (${row},${col})`,
+            ]);
+        });
+        // TODO: TEST UNAUTHORIZED
+    });
+
+    describe('Unmarking', () => {
+        beforeEach(() => {
+            room.board[mockMarkAction.payload.row][
+                mockMarkAction.payload.col
+            ].completedPlayers = [mockTokenPayload.playerId];
+            room.players
+                .get(mockTokenPayload.playerId)
+                ?.mark(mockMarkAction.payload.row, mockMarkAction.payload.col);
+        });
+
+        it('Unmarks the correct cell if it is marked', () => {
+            room.handleUnmark(mockUnmarkAction, mockTokenPayload);
+            let completedPlayers =
+                room.board[mockMarkAction.payload.row][
+                    mockMarkAction.payload.col
+                ].completedPlayers;
+            expect(completedPlayers.length).toBe(0);
+        });
+
+        it('Sends a cell message update', () => {
+            const playerSendSpy = jest.spyOn(
+                room.players.get(mockTokenPayload.playerId)!,
+                'sendMessage',
+            );
+            const player2SendSpy = jest.spyOn(
+                room.players.get(mockTokenPayloadPlayer2.playerId)!,
+                'sendMessage',
+            );
+            room.handleUnmark(mockUnmarkAction, mockTokenPayload);
+            const cellUpdateMessage: ServerMessage = {
+                action: 'cellUpdate',
+                row: mockMarkAction.payload.row,
+                col: mockMarkAction.payload.col,
+                cell: room.board[mockMarkAction.payload.row][
+                    mockMarkAction.payload.col
+                ],
+                players: room.getPlayers(),
+            };
+            expect(playerSendSpy).toHaveBeenCalledWith(cellUpdateMessage);
+            expect(player2SendSpy).toHaveBeenCalledWith(cellUpdateMessage);
+        });
+
+        it('Sends a chat message', () => {
+            const sendChatSpy = jest.spyOn(room, 'sendChat');
+            const player = room.players.get(mockTokenPayload.playerId)!;
+            const { row, col } = mockMarkAction.payload;
+            room.handleUnmark(mockUnmarkAction, mockTokenPayload);
+            expect(sendChatSpy).toHaveBeenCalledTimes(1);
+            expect(sendChatSpy).toHaveBeenCalledWith([
+                {
+                    contents: player.nickname,
+                    color: player.color,
+                },
+                ` unmarked ${room.board[row][col].goal.goal} (${row},${col})`,
+            ]);
+        });
+        // TODO: TEST UNAUTHORIZED
+    });
 });
