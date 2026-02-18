4f75446e22cb90641ee56f8ab847ba73a3458635
cjs8487 <cjschaefer1999@gmail.com>
2026-02-17 21:48:43 -0600

    add game cover to room list on homepage

--- web/src/components/ActiveRoomList.tsx
+++ web/src/components/ActiveRoomList.tsx
@@ -2,6 +2,7 @@ import {
     Card,
     CardActionArea,
     CardContent,
+    CardMedia,
     List,
     ListItem,
     Typography,
@@ -9,6 +10,7 @@ import {
 import { RoomData } from '@playbingo/types';
 import { connection } from 'next/server';
 import { serverGet } from '../app/ServerUtils';
+import { getFullUrl } from '../lib/Utils';
 
 async function getRooms(): Promise<RoomData[]> {
     const res = await serverGet('/api/rooms');
@@ -34,12 +36,30 @@ export default async function ActiveRoomList() {
         );
     }
 
+    console.log(rooms);
+
     return (
         <List>
             {rooms.map((room) => (
                 <ListItem key={room.slug}>
                     <Card variant="outlined">
-                        <CardActionArea href={`/rooms/${room.slug}`}>
+                        <CardActionArea
+                            href={`/rooms/${room.slug}`}
+                            sx={{
+                                display: 'flex',
+                            }}
+                        >
+                            <CardMedia
+                                component="img"
+                                image={getFullUrl(
+                                    `/media/gameCover/${room.gameSlug}`,
+                                )}
+                                sx={{
+                                    width: '120px',
+                                    aspectRatio: '11 / 16',
+                                    objectFit: 'cover',
+                                }}
+                            />
                             <CardContent>
                                 <Typography variant="h5">
                                     {room.name}
