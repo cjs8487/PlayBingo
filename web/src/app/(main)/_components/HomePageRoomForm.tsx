bfa8fdb1313081e6bf8479c1055b044827f2dc50
cjs8487 <cjschaefer1999@gmail.com>
2026-07-05 23:16:33 -0500

    make room password a password field

--- web/src/app/(main)/_components/HomePageRoomForm.tsx
+++ web/src/app/(main)/_components/HomePageRoomForm.tsx
@@ -94,6 +94,7 @@ export default function HomePageRoomForm() {
                             name="password"
                             label="Password"
                             fullWidth
+                            type="password"
                         />
                         <FormikTextField
                             id="player-nickname"
